6b0c3ad870ccc96ef0de7851cccd9044a449f178
Belle <bofiss@gmail.com>
2021-03-21 09:43:20 +0100

    first commit

---
+++ my-sfg-pet-clinic/src/test/java/belle/springframework/mysfgpetclinic/MySfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package belle.springframework.mysfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
